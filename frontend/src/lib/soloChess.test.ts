7813cff13b6d92a427b32977b8bd7034e3ff892b
MaximeKELI <presidentnetero01@gmail.com>
2026-06-27 21:53:06 +0000

    All coders

--- frontend/src/lib/soloChess.test.ts
+++ frontend/src/lib/soloChess.test.ts
@@ -8,7 +8,7 @@ describe("soloChess", () => {
   });
 
   it("soloVictory quand une pièce reste", () => {
-    const c = new Chess("4Q3/8/8/8/8/8/8/8 w - - 0 1");
+    const c = new Chess("7Q/8/8/8/8/8/8/8 w - - 0 1");
     expect(soloVictory(c)).toBe(true);
   });
 
