2702ff328557fcedaff14952423387d07d1d81e4
MaximeKELI <presidentnetero01@gmail.com>
2026-06-28 14:13:16 +0000

    All coders

--- frontend/src/hooks/useGameWebSocket.ts
+++ frontend/src/hooks/useGameWebSocket.ts
@@ -186,8 +186,6 @@ export function useMatchmakingWebSocket(
   const search = useCallback(() => {
     const token = Cookies.get("access_token");
     if (!token) return;
-
-    if (wsRef.current) {
       wsRef.current.close();
     }
 
