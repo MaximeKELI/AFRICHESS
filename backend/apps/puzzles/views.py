651384f582afc4e4e44c5d5cbc5b0408a71db0f7
MaximeKELI <presidentnetero01@gmail.com>
2026-07-12 17:34:34 +0000

    Recuperation de compte

--- backend/apps/puzzles/views.py
+++ backend/apps/puzzles/views.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
+from django.db.models import Count, Q
 from django.utils import timezone
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
